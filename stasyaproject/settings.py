599a4cc3ea5e9bb4e84e63acc2fab211f5726e19
danil <danil.ceregin@bk.ru>
2022-01-16 16:47:57 +0600

    Changes

--- stasyaproject/settings.py
+++ stasyaproject/settings.py
@@ -133,7 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
